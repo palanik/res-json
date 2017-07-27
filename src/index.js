505469aac9e8b7fd5f0469f59c7ad164beb55d18
palanik <palani.kumanan@gmail.com>
2017-07-26 23:26:39 -0400

    output in utf8, callback query string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import url from 'url';
+
 function json(replacer, space, callback = 'callback') {
   const stringify = (replacer || space)
     ? (val => JSON.stringify(val, replacer, space))
@@ -6,20 +8,26 @@ function json(replacer, space, callback = 'callback') {
   return ((req, res, next) => {
     res.json = ((val) => {
       const body = stringify(val);
-      if (!res.getHeader('Content-Type')) {
-        res.setHeader('Content-Type', 'application/json');
+      if (!res.getHeader('content-type')) {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
       }
 
-      res.end(body);
+      res.end(body, 'utf8');
     });
 
     res.jsonp = ((val) => {
-      const body = stringify(val);
-      if (!res.getHeader('Content-Type')) {
-        res.setHeader('Content-Type', 'text/javascript');
-      }
+      const reqUrl = url.parse(req.url, true);
+      if (reqUrl.query[callback]) {
+        const cb = reqUrl.query[callback];
+        const body = stringify(val);
+        if (!res.getHeader('content-type')) {
+          res.setHeader('Content-Type', 'text/javascript; charset=utf-8');
+        }
 
-      res.end(`typeof ${callback} === 'function' && ${callback}(${body});`);
+        res.end(`typeof ${cb} === 'function' && ${cb}(${body});`, 'utf8');
+      } else {
+        res.json(val); // No callback query string? Fallback to json
+      }
     });
 
     next();
@@ -27,3 +35,4 @@ function json(replacer, space, callback = 'callback') {
 }
 
 export default json;
+module.exports = json;
